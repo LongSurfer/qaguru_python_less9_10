ae9755bd9f04e6e81b115586071c524adc12e07b
Alex Evans <ivanov2avi@yandex.ru>
2022-09-01 19:35:48 +0300

    default browser version

--- tests/demoqa_tests/test_students_form.py
+++ tests/demoqa_tests/test_students_form.py
@@ -1,30 +1,11 @@
 import allure
 import self as self
 from selene.support.conditions import have
-from selene.support.shared import browser
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selene import command, be
 
-from utils import attach
 
-
-def test_register_student():
-
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
-    browser.config.driver = driver
+def test_register_student(setup_browser):
+    browser = setup_browser
 
     with allure.step('Open students registration form'):
         browser.open('https://demoqa.com/automation-practice-form')
@@ -62,29 +43,10 @@ def test_register_student():
     with allure.step('Assert'):
         browser.element('#example-modal-sizes-title-lg').should(have.exact_text('Thanks for submitting the form'))
 
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
 
 
-def test_fail_students_form():
-
-    options = Options()
-    selenoid_capabilities = {
-        "browserName": "chrome",
-        "browserVersion": "100.0",
-        "selenoid:options": {
-            "enableVNC": True,
-            "enableVideo": True
-        }
-    }
-    options.capabilities.update(selenoid_capabilities)
-    driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
-        options=options)
-    browser.config.driver = driver
-
+def test_fail_students_form(setup_browser):
+    browser = setup_browser
 
     with allure.step('Open students registration form'):
         browser.open('https://demoqa.com/automation-practice-form')
@@ -107,7 +69,3 @@ def test_fail_students_form():
                 return True
 
 
-    attach.add_html(browser)
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_video(browser)
